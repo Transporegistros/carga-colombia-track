fa7c1beec67fd03e653370c7dcb7d037cc593de7
Transporegistros <transporegistrosplus@gmail.com>
2025-05-17 15:24:55 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  plugins: [
-    react(),          // añade soporte React/JSX
-    tsconfigPaths()   // resuelve los paths definidos en tsconfig.json
-  ],
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   server: {
-    port: 3000
+    host: "::",
+    port: 8080,
   },
-  define: {
-    'process.env': {} // para librerías que lo esperen
-  }
-})
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
